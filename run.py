d513d3f84eb2cf32ee41103a19e4bc65c54ee853
Faizan Shaikh <160400004+Faizan21-Analyst@users.noreply.github.com>
2025-08-17 20:03:01 +0530

    run.py

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import os
 from app import create_app
 
+# Create the Flask app via factory
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # use Render's assigned port
-    app.run(host="0.0.0.0", port=port)
+    # Render provides PORT dynamically
+    port = int(os.environ.get("PORT", 5000))  # fallback for local
+    app.run(host="0.0.0.0", port=port, debug=True)
